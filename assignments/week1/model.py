c8d7234e111185a77fc265db5fdc8f968a158a3e
gugababa <anshuswain99@gmail.com>
2023-01-31 16:34:45 -0500

    Updated format

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -7,41 +7,42 @@ class LinearRegression:
     """
 
     w: np.ndarray
-    b: float 
+    b: float
 
     def __init__(self) -> None:
         self.w = np.zeros(1)
         self.b = np.zeros(1)
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        '''Performs fit of the model
+        """Performs fit of the model
         Args:
             X = matrix of features (d) and examples (n)
             Y = vector of targets (n)
 
         Returns:
             None
-        '''
+        """
         # add a bias term to X matrix
-        X = np.hstack((X, np.ones(X.shape[0]).reshape(-1,1)))
+        X = np.hstack((X, np.ones(X.shape[0]).reshape(-1, 1)))
         self.weights = np.zeros(X.shape[1])
-        if np.linalg.det(X.T@X) != 0:
-            self.weights = np.linalg.inv(X.T@X)@X.T@y
+        if np.linalg.det(X.T @ X) != 0:
+            self.weights = np.linalg.inv(X.T @ X) @ X.T @ y
         else:
             print("Matrix is singular. No closed form solution.")
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        '''
-            Predict the targets determined by the weights
-            Args:
-                X = matrix of features (d) and examples (n)
-            Return:
-                y = vector of predicted targets
+        """
+        Predict the targets determined by the weights
+        Args:
+            X = matrix of features (d) and examples (n)
+        Return:
+            y = vector of predicted targets
 
-        '''
+        """
         self.b = self.weights[-1]
         self.w = self.weights[:-1]
-        return X@self.w + self.b
+        return X @ self.w + self.b
+
 
 class GradientDescentLinearRegression(LinearRegression):
     """
@@ -50,11 +51,13 @@ class GradientDescentLinearRegression(LinearRegression):
 
     def gradient(self, X, y, w):
 
-        return (2/X.shape[0])*X.T@(X@w - y)
+        return (2 / X.shape[0]) * X.T @ (X @ w - y)
 
-    def fit(self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000)-> None:
+    def fit(
+        self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
+    ) -> None:
 
-        '''Performs fit of the model
+        """Performs fit of the model
         Args:
             X = matrix of features (d) and examples (n)
             Y = vector of targets (n)
@@ -62,18 +65,17 @@ class GradientDescentLinearRegression(LinearRegression):
             epochs = number of iterations
         Returns:
             None
-        '''
-        if (X[:,-1] != np.ones(X.shape[0])).all():
-            X = np.hstack((X, np.ones(X.shape[0]).reshape(-1,1)))
-        
-        self.w = np.zeros(X.shape[1]).reshape(-1,1)
+        """
+        if (X[:, -1] != np.ones(X.shape[0])).all():
+            X = np.hstack((X, np.ones(X.shape[0]).reshape(-1, 1)))
+
+        self.w = np.zeros(X.shape[1]).reshape(-1, 1)
         if y.shape != X.shape[0]:
-            y = y.reshape(-1,1)
+            y = y.reshape(-1, 1)
 
         for i in range(epochs):
-            
-            self.w -= lr*self.gradient(X,y,self.w)
 
+            self.w -= lr * self.gradient(X, y, self.w)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -86,4 +88,4 @@ class GradientDescentLinearRegression(LinearRegression):
             np.ndarray: The predicted output.
 
         """
-        return X@self.w[:-1] + self.w[-1]
+        return X @ self.w[:-1] + self.w[-1]
