c07d7f4817fca83c54b8e1249f1f584ff9fcc06a
gugababa <anshuswain99@gmail.com>
2023-02-19 18:50:26 -0500

    Submission

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,25 +6,22 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
+
     batch_size = 32
     num_epochs = 50
-    initial_learning_rate = 2e-1
+    initial_learning_rate = 8e-3
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "gamma": 0.1,
-        "stepsize": 5,
+        'gamma' : 0.1, 'stepsize': 5
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.SGD(
-        model.parameters(),
-        lr=CONFIG.initial_learning_rate,
-        weight_decay=CONFIG.initial_weight_decay,
-        momentum=0.9,
+        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay, momentum = 0.9
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,16 @@
 from typing import List
 
 from torch.optim.lr_scheduler import _LRScheduler
+import numpy as np
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+
+    """
+    Implementation of a custom LR scheduler that inherits from PyTorch _LRScheduler
+    """
+
+    def __init__(self, optimizer, gamma, stepsize, last_epoch=-1):
         """
         Create a new scheduler.
 
@@ -12,13 +18,19 @@ class CustomLRScheduler(_LRScheduler):
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
+        self.gamma = gamma
+        self.step_size = stepsize
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        """
+        Create an exponential scheduler, with the learning rate decaying exponentially with square root of gamma value
+        """
+
+        if (self.last_epoch == 0) or (self.last_epoch % self.step_size != 0):
+            return [group["lr"] for group in self.optimizer.param_groups]
+
+        return [i["lr"] * (self.gamma) for i in self.optimizer.param_groups]
